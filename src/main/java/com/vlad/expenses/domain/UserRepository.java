586a2fd6e00874382015ed65c71ac699e9bb0edb
Vladislav Altunin <vlad@Vladislavs-MacBook-Pro.local>
2022-12-07 20:37:23 +0200

    user repo created

---
+++ src/main/java/com/vlad/expenses/domain/UserRepository.java
@@ -0,0 +1,12 @@
+package com.vlad.expenses.domain;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer>{
+	Optional<User> findByName(@Param("username") String username);
+}
