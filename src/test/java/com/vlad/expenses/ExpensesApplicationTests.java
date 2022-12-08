205980a73e56042cdc09c9ba5031567313eaf3fa
Vladislav Altunin <vlad@Vladislavs-MacBook-Pro.local>
2022-12-08 13:14:06 +0200

    smoke tests added

--- src/test/java/com/vlad/expenses/ExpensesApplicationTests.java
+++ src/test/java/com/vlad/expenses/ExpensesApplicationTests.java
@@ -1,13 +1,30 @@
 package com.vlad.expenses;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.vlad.expenses.web.ExpenseController;
+import com.vlad.expenses.web.ExpenseRestController;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class ExpensesApplicationTests {
-
+	
+	@Autowired
+	private ExpenseController controller;
+	
+	@Autowired
+	private ExpenseRestController restController;
+	
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
+		assertThat(restController).isNotNull();
 	}
 
 }
